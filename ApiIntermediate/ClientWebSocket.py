b04a811edb3a1ccc76d810f1b1357bacddf78732
kenneth <kenneth.pulgarinvilla00@gmail.com>
2022-07-08 01:51:50 -0500

    correccion de error al arrancar docker

--- ApiIntermediate/ClientWebSocket.py
+++ ApiIntermediate/ClientWebSocket.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from os import getenv
 from utils.logger_base import log
 from requests.structures import CaseInsensitiveDict
+from busypie import wait, SECOND
 
 load_dotenv()
 URL_SEND = getenv("URL_CRUD")
@@ -14,19 +15,20 @@ USERNAME = getenv("SUPERADMIN")
 PASSWORD = getenv("PSWD_ADMIN")
 
 
-async def request_token():
+def request_token():
     headers_get = CaseInsensitiveDict()
     print(USERNAME)
     print(PASSWORD)
     headers_get["username"] = "admin"
     headers_get["password"] = "admin123"
     response_token = requests.get(URL_SEND + "token/", headers=headers_get)
-    log.debug(response_token)
-    return dict(response_token.json())
+    log.debug(response_token.json())
+    return response_token
 
 
 async def save_register_with_device():
-    token = await request_token()
+    await_token()
+    token = dict(request_token().json())
     async with websockets.connect(URL_WEBSOCKET, extra_headers={"Authorization": SECRET}) as websocket:
         headers = CaseInsensitiveDict()
         headers["Authorization"] = f"Bearer {token['token']}"
@@ -36,3 +38,8 @@ async def save_register_with_device():
             x = requests.post(URL_SEND, json=js, headers=headers)
             log.debug(x.status_code)
             log.debug(x.text)
+
+
+def await_token():
+    wait().at_most(15, SECOND).poll_interval(3, SECOND).with_description('dont get the token').until(
+        lambda: request_token().status_code == 200)
--- README.md
+++ README.md
@@ -10,11 +10,8 @@ Reto tecnico de microservicios
 Para ejecutar el proyecto debemos seguir los siguientes pasos:
 1. Descargar el repositorio
 2. Deberemos instalar docker en caso de no tenerlo en nuestra maquina
-3. Ejecutar el comando **docker-compose build** en la raiz de este proyecto el cual creará
-las imagenes a ejecutar posteriormente
-4. Ejecutar el comando **docker-compose up** en la raiz del proyecto para arrancar los servicios
-(esto debe ser asi porque en caso contrario la api intermediaria no alcanza a detecetar los servicios)
-5. Automaticamente activará los contenedores necesarios para ejecutar el proyecto
+3. Ejecutar el comando **docker-compose up** en la raiz del proyecto para arrancar los servicios
+4. Automaticamente activará los contenedores necesarios y ejecutará el proyecto
 
 **_Contenedores de docker y puertos_**
 | servicio | contenedor docker| puerto |
