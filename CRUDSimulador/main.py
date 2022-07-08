03d2aca43615eb0e7bff1f440eb0be760e02e02a
kenneth <kenneth.pulgarinvilla00@gmail.com>
2022-07-08 14:54:26 -0500

    modificacion validacion sin bd para correcion de despliegue

--- CRUDSimulador/main.py
+++ CRUDSimulador/main.py
@@ -1,8 +1,5 @@
-import sqlalchemy
 from fastapi import FastAPI
 from routes.CrudSimulador import simulator
-from config.DbHandler import conn
-from models.Dao.AdminDao import admin
 from dotenv import load_dotenv
 from os import getenv
 from utils.jwt.jwt_handler import get_token
@@ -16,19 +13,4 @@ app.include_router(simulator)
 app.include_router(token, prefix='/token')
 
 
-def insert_admin():
-    try:
-        data = {'user': USER, 'password': PASSWORD}
-        conn.execute(admin.insert().values(data))
-        return True
-    except sqlalchemy.exc as e:
-        return False
-    except Exception:
-        return False
 
-
-@app.on_event('startup')
-def create_admin():
-    result = False
-    while result:
-        result = insert_admin()
--- CRUDSimulador/routes/token.py
+++ CRUDSimulador/routes/token.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Header
 from fastapi.responses import JSONResponse
-from config.DbHandler import conn
-from models.Dao.AdminDao import admin
 from utils.jwt.jwt_handler import validate_token, get_token
 from starlette.status import HTTP_200_OK, HTTP_403_FORBIDDEN
+from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv()
 token = APIRouter()
 
 
@@ -13,15 +14,7 @@ def generate_token(username: str = Header(None), password: str = Header(None)):
     if username == None or password == None:
         return JSONResponse(status_code=HTTP_403_FORBIDDEN, content="forhiben")
     else:
-        result = conn.execute(admin.select().where(admin.c.user == username)).first()
-        if result:
-            response = validate_token(result[1], output=True)
-            if isinstance(response, JSONResponse):
-                return response
-            else:
-                if response['password'] == password:
-                    return JSONResponse(status_code=HTTP_200_OK, content={"token": get_token({'password': password})})
-                else:
-                    return JSONResponse(status_code=HTTP_403_FORBIDDEN, content="forhiben")
+        if username == getenv("SUPERADMIN") and password == getenv("PSWD_ADMIN"):
+            return JSONResponse(status_code=HTTP_200_OK, content={"token": get_token({'password': password})})
         else:
             return JSONResponse(status_code=HTTP_403_FORBIDDEN, content="forhiben")
